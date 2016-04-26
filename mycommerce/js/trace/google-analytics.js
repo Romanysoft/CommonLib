5cfe25c3952fe9db20d99c40a46da1dd9f2bfca4
iansun <lauer3912@gmail.com>
2016-04-26 09:52:08 +0800

    更新

--- mycommerce/js/trace/google-analytics.js
+++ mycommerce/js/trace/google-analytics.js
@@ -4,60 +4,56 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    $(document).ready(function() {
-        try {
-              // all traceid  
-              ga('create', 'UA-54045904-3', 'auto');
 
+    try {
+          // all traceid  
+          ga('create', 'UA-54045904-3', 'auto');
+
+          
+          //-------------------------------------------------
+          //see some trace id
+          var _href = window.location.href,
+              _mac = " for Mac", _win = "  for Windows", _linux = " for Linux",
+              _linkIdList = [    // 
+                  "RF201603221800PTJ",
+                  "RF201604071748PTJ"
+              ]
+          try{
+              _lowerHref = _href.toLowerCase();
               
-              //-------------------------------------------------
-              //see some trace id
-              var _href = window.location.href,
-                  _mac = " for Mac", _win = "  for Windows", _linux = " for Linux",
-                  _linkIdList = [    // 
-                      "RF201603221800PTJ",
-                      "RF201604071748PTJ"
-                  ]
-              try{
-                  _lowerHref = _href.toLowerCase();
+              //eg. https://shopper.mycommerce.com/checkout/cart/new/55399-17
+              // find product
+               var productList = $('.dr_cart_product strong');
+               $.each(productList, function(index, ele){
+                   var productName = $(ele).html();
+                   ga('ec'+productName, {'date': (new Date()).toGMTString()});
+               });
+
+              // find linkId,
+              var fnc_findLinkID = function(_lowerHref){
+                  var found = false, linkId = "";
+                  $.each(_linkIdList, function(i, item){
+                    var _lowerItem = 'linkid='+item.toLowerCase();
+                    if(_lowerHref.indexOf(_lowerItem) > -1){
+                        linkId = item;
+                        found = true;
+                        return false;
+                    }
+                    
+                  });
                   
-                  //eg. https://shopper.mycommerce.com/checkout/cart/new/55399-17
-                  // find product
-                   var productList = $('.dr_cart_product strong');
-                   $.each(productList, function(index, ele){
-                       var productName = $(ele).html();
-                       ga('ec'+productName, {'date': (new Date()).toGMTString()});
-                   });
+                  return linkId;
+              };
 
-                  // find linkId,
-                  var fnc_findLinkID = function(_lowerHref){
-                      var found = false, linkId = "";
-                      $.each(_linkIdList, function(i, item){
-                        var _lowerItem = 'linkid='+item.toLowerCase();
-                        if(_lowerHref.indexOf(_lowerItem) > -1){
-                            linkId = item;
-                            found = true;
-                            return false;
-                        }
-                        
-                      });
-                      
-                      return linkId;
-                  };
+              //////////////////////////////////////////////////////////////////////
+              ga('ec:linkID', fnc_findLinkID(_lowerHref));
 
-                  //////////////////////////////////////////////////////////////////////
-                  ga('ec:linkID', fnc_findLinkID(_lowerHref));
-
-              }catch(e){console.error(e);}
-              
-              ga('send', 'pageview');
-              //ga('send',{hitType:'pageview', page: location.pathname});
-              
-              
-        } catch (err) {
-            console.error(err);
-        }
-    })
-
+          }catch(e){console.error(e);}
+          
+          ga('send',{hitType:'pageview', page: location.pathname});
+          
+    } catch (err) {
+        console.error(err);
+    }
 
 })();
