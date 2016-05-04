a154c66653c967a8e57d0eaa59c74cd911b9fd10
iansun <lauer3912@gmail.com>
2016-05-05 07:23:21 +0800

    Update

--- c_sellers/js/go.js
+++ c_sellers/js/go.js
@@ -9,7 +9,7 @@
         a.async = 1;
         a.src = g;
         m.parentNode.insertBefore(a, m)
-    // })(window, document, 'script', '//www.google-analytics.com/analytics_debug.js', 'ga');
+        // })(window, document, 'script', '//www.google-analytics.com/analytics_debug.js', 'ga');
     })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 
@@ -52,80 +52,80 @@
             ga('create', 'UA-54045904-5', 'auto');
             ga(function(tracker) {
                 console.log(tracker.get('clientId'));
-
                 core_process();
             });
 
+            //see some trace id
+            var _href = window.location.href,
+                _mac = " for Mac",
+                _win = "  for Windows",
+                _linux = " for Linux",
+                foundProductID = "",
+                foundProdutName = "",
+                foundLinkID = "",
+                _productsMap = {
+                    "55399-1": "SpeedTest" + _mac,
+                    "55399-2": "BarcodeUV" + _mac,
+                    "55399-3": "BarcodePro" + _mac,
+                    "55399-4": "AN PDF" + _mac,
+                    "55399-5": "Color Converter" + _mac,
+                    "55399-6": "CSSDesigner" + _mac,
+                    "55399-8": "GraphSketcher" + _mac,
+                    "55399-9": "CSVKiller" + _mac,
+                    "55399-10": "TryToMp3" + _mac,
+                    "55399-11": "TryToAVI" + _mac,
+                    "55399-12": "TryToAC3" + _mac,
+                    "55399-13": "TryToMPG" + _mac,
+                    "55399-14": "TryToWav" + _mac,
+                    "55399-15": "TryToSWF" + _mac,
+                    "55399-16": "ImageMinify" + _mac,
+                    "55399-17": "DataStorm" + _mac,
+                    "55399-18": "Batch File Translater" + _mac,
+                    "55399-19": "5BR3C" + _mac,
+                    "55399-20": "ExifImage" + _mac,
+                    "55399-21": "SRT2" + _mac,
+                    "55399-22": "TryToFLV" + _mac,
+                    "55399-23": "TryToMp4" + _mac,
+                    "55399-24": "TryToAAC" + _mac,
+                    "55399-25": "TryToAIFF" + _mac,
+                    "55399-26": "TryToALAC" + _mac,
+                    "55399-27": "TryToAMR" + _mac,
+                    "55399-28": "TryToOGG" + _mac,
+                    "55399-29": "TryToOpus" + _mac,
+                    "55399-30": "TryToWMA" + _mac,
+                    "55399-31": "Css Sprite Helper" + _win,
+                    "55399-32": "MarkdownD" + _mac,
+                    "55399-33": "SudokuMM" + _mac,
+                    "55399-34": "Try to translate" + _mac,
+                    "55399-35": "CurrencyCalc" + _mac,
+                    "55399-36": "LET" + _mac,
+                    "55399-37": "MarkdownD" + _win,
+                    "55399-38": "MarkdownD" + _linux,
+                    "55399-39": "Try to translate" + _win,
+                    "55399-40": "Try to translate" + _linux,
+                    "55399-41": "DataStorm" + _win,
+                    "55399-42": "DataStorm" + _linux,
+                    "55399-43": "XLS2csv" + _mac,
+                    "55399-44": "CSSDesigner" + _win,
+                    "55399-45": "CSSDesigner" + _linux,
+
+                },
+                _linkIdList = [ //
+                    //// --- 01
+                    "RF201501221800RT",
+                    "RF201502221800RT",
+                    "RF201503221800RT",
+                    "RF201504221800RT",
+                    "RF201505221800RT",
+                    "RF201506221800RT",
+                    "AnnreChen",
+
+                    //// --- 02
+                    "RF201603221800PTJ",
+                    "RF201604071748PTJ"
+                ];
+
             var core_process = function() {
-                //see some trace id
-                var _href = window.location.href,
-                    _mac = " for Mac",
-                    _win = "  for Windows",
-                    _linux = " for Linux",
-                    foundProductID = "",
-                    foundProdutName = "",
-                    foundLinkID = "",
-                    _productsMap = {
-                        "55399-1": "SpeedTest" + _mac,
-                        "55399-2": "BarcodeUV" + _mac,
-                        "55399-3": "BarcodePro" + _mac,
-                        "55399-4": "AN PDF" + _mac,
-                        "55399-5": "Color Converter" + _mac,
-                        "55399-6": "CSSDesigner" + _mac,
-                        "55399-8": "GraphSketcher" + _mac,
-                        "55399-9": "CSVKiller" + _mac,
-                        "55399-10": "TryToMp3" + _mac,
-                        "55399-11": "TryToAVI" + _mac,
-                        "55399-12": "TryToAC3" + _mac,
-                        "55399-13": "TryToMPG" + _mac,
-                        "55399-14": "TryToWav" + _mac,
-                        "55399-15": "TryToSWF" + _mac,
-                        "55399-16": "ImageMinify" + _mac,
-                        "55399-17": "DataStorm" + _mac,
-                        "55399-18": "Batch File Translater" + _mac,
-                        "55399-19": "5BR3C" + _mac,
-                        "55399-20": "ExifImage" + _mac,
-                        "55399-21": "SRT2" + _mac,
-                        "55399-22": "TryToFLV" + _mac,
-                        "55399-23": "TryToMp4" + _mac,
-                        "55399-24": "TryToAAC" + _mac,
-                        "55399-25": "TryToAIFF" + _mac,
-                        "55399-26": "TryToALAC" + _mac,
-                        "55399-27": "TryToAMR" + _mac,
-                        "55399-28": "TryToOGG" + _mac,
-                        "55399-29": "TryToOpus" + _mac,
-                        "55399-30": "TryToWMA" + _mac,
-                        "55399-31": "Css Sprite Helper" + _win,
-                        "55399-32": "MarkdownD" + _mac,
-                        "55399-33": "SudokuMM" + _mac,
-                        "55399-34": "Try to translate" + _mac,
-                        "55399-35": "CurrencyCalc" + _mac,
-                        "55399-36": "LET" + _mac,
-                        "55399-37": "MarkdownD" + _win,
-                        "55399-38": "MarkdownD" + _linux,
-                        "55399-39": "Try to translate" + _win,
-                        "55399-40": "Try to translate" + _linux,
-                        "55399-41": "DataStorm" + _win,
-                        "55399-42": "DataStorm" + _linux,
-                        "55399-43": "XLS2csv" + _mac,
-                        "55399-44": "CSSDesigner" + _win,
-                        "55399-45": "CSSDesigner" + _linux,
-
-                    },
-                    _linkIdList = [ //
-                        //// --- 01
-                        "RF201501221800RT",
-                        "RF201502221800RT",
-                        "RF201503221800RT",
-                        "RF201504221800RT",
-                        "RF201505221800RT",
-                        "RF201506221800RT",
-                        "AnnreChen",
-
-                        //// --- 02
-                        "RF201603221800PTJ",
-                        "RF201604071748PTJ"
-                    ]
                 try {
                     _lowerHref = _href.toLowerCase();
 
@@ -168,17 +168,17 @@
                         hitType: 'event',
                         eventCategory: 'OrderProduct',
                         eventAction: 'traceProduct',
-                        eventLabel: foundProductID + " ## " +foundProdutName
+                        eventLabel: foundProductID + " ## " + foundProdutName
                     });
 
                     ga('send', {
                         hitType: 'event',
-                        eventCategory: foundProductID + " ## " +foundProdutName,
+                        eventCategory: foundProductID + " ## " + foundProdutName,
                         eventAction: 'gotoPurchase',
                         eventLabel: foundProdutName + " || " + (new Date()).toUTCString()
                     });
 
-                    if("" !== $.trim(foundLinkID)){
+                    if ("" !== $.trim(foundLinkID)) {
                         ga('send', {
                             hitType: 'event',
                             eventCategory: foundLinkID,
@@ -197,35 +197,34 @@
                 } catch (e) {
                     console.error(e);
                 }
-
-                ga('send', {
-                    hitType: 'pageview',
-                    page: location.pathname
-                });
-
-                //------------------------------------------------
-                //build redircet url.
-                //eg. org: http://romanysoft.github.io/CommonLib/sellers.html?pid=55399-1&quantity=1&linkid=RF201603221800PTJ
-                //    dest: https://shopper.mycommerce.com/checkout/cart/add/55399-1?quantity=1&linkid=RF201603221800PTJ
-                var destUrl = "https://shopper.mycommerce.com/checkout/cart/add/",
-                    orgUrlRplace = "http://romanysoft.github.io/CommonLib/sellers.html?pid=";
-
-                //go url
-                var baseUrl = _lowerHref.replace(orgUrlRplace.toLowerCase(), destUrl);
-
-
-                if (baseUrl.toLowerCase().indexOf(orgUrlRplace.toLowerCase()) == -1 && baseUrl.toLowerCase() !== _lowerHref) {
-                    baseUrl = baseUrl.replace('&quantity=', '?quantity=');
-
-                    if(foundProductID == ""){
-                        window.location.href = "https://shopper.mycommerce.com/checkout/cart/add/55399-1?quantity=1";
-                    }else{
-                        window.location.href = baseUrl;
-                    }
-
-                }
             };
 
+            ga('send', {
+                hitType: 'pageview',
+                page: location.pathname
+            });
+
+            //------------------------------------------------
+            //build redircet url.
+            //eg. org: http://romanysoft.github.io/CommonLib/sellers.html?pid=55399-1&quantity=1&linkid=RF201603221800PTJ
+            //    dest: https://shopper.mycommerce.com/checkout/cart/add/55399-1?quantity=1&linkid=RF201603221800PTJ
+            var destUrl = "https://shopper.mycommerce.com/checkout/cart/add/",
+                orgUrlRplace = "http://romanysoft.github.io/CommonLib/sellers.html?pid=";
+
+            //go url
+            var baseUrl = _lowerHref.replace(orgUrlRplace.toLowerCase(), destUrl);
+
+
+            if (baseUrl.toLowerCase().indexOf(orgUrlRplace.toLowerCase()) == -1 && baseUrl.toLowerCase() !== _lowerHref) {
+                baseUrl = baseUrl.replace('&quantity=', '?quantity=');
+
+                if (foundProductID == "") {
+                    window.location.href = "https://shopper.mycommerce.com/checkout/cart/add/55399-1?quantity=1";
+                } else {
+                    window.location.href = baseUrl;
+                }
+
+            }
 
         } catch (err) {
             console.error(err);
