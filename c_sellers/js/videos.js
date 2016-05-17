06767c92f0cd3d5529e3df732062db105c06a470
iansun <lauer3912@gmail.com>
2016-05-16 19:21:11 -0700

    更新

--- c_sellers/js/videos.js
+++ c_sellers/js/videos.js
@@ -1,36 +1,35 @@
 (function($) {
-        
-    var _baseUrl = "/videos/sellers/";        
+              
     $.RTY_videos = [
         {
           category:"FaceBook", 
           name:"FaceBook上在小组内卖东西.mp4", 
           description:"在FaceBook上推广销售帖子及卖东西是一项有效的推广方式", 
-          url: _baseUrl + "facebook/" +"FaceBook上在小组内卖东西.mp4"
+          url: "http://www.filefactory.com/file/1qjo7li28pgj/FaceBook%u4E0A%u5728%u5C0F%u7EC4%u5185%u5356%u4E1C%u897F.mp4"
         },
         {
           category:"FaceBook", 
           name:"FaceBook小组如何找到卖东西选项.mp4", 
           description:"有的时候，加入小组内卖东西选项找不到，可以这样试试", 
-          url:_baseUrl + "facebook/" +"FaceBook小组如何找到卖东西选项.mp4"
+          url:"http://www.filefactory.com/file/3o3pif83mm7v/FaceBook%u5C0F%u7EC4%u5982%u4F55%u627E%u5230%u5356%u4E1C%u897F%u9009%u9879.mp4"
         },  
         {
           category:"FaceBook", 
           name:"FaceBook加入更多小组.mp4", 
           description:"好产品要推广，加入更多小组，以货品出售小组查找为例。", 
-          url:_baseUrl + "facebook/" +"FaceBook加入更多小组.mp4"
+          url:"http://www.filefactory.com/file/6e7eupqlmyhp/FaceBook%u52A0%u5165%u66F4%u591A%u5C0F%u7EC4.mp4"
         },
         {
           category:"FaceBook", 
           name:"FaceBook如何添加更多小组.mp4", 
           description:"针对没有货品出售小组的同仁，可以这样添加更多小组", 
-          url:_baseUrl + "facebook/" +"FaceBook如何添加更多小组.mp4"
+          url:"http://www.filefactory.com/file/1smiiafvpyhh/FaceBook%u5982%u4F55%u6DFB%u52A0%u66F4%u591A%u5C0F%u7EC4.mp4"
         },      
         {
           category:"FaceBook", 
           name:"FaceBook小组WindowsMac如何将你的好友添加进来.mp4", 
           description:"我们自己的小组，希望将你的好友都申请加入进来，方便推广", 
-          url:_baseUrl + "facebook/" +"FaceBook小组WindowsMac如何将你的好友添加进来.mp4"
+          url:"http://www.filefactory.com/file/4uh0ga02cn11/FaceBook%u5C0F%u7EC4WindowsMac%u5982%u4F55%u5C06%u4F60%u7684%u597D%u53CB%u6DFB%u52A0%u8FDB%u6765.mp4"
         },
             
             
@@ -39,7 +38,7 @@
           category:"Office", 
           name:"产品代理销售在线工具.mp4", 
           description:"如何使用产品在线销售工具?", 
-          url:_baseUrl + "office/" +"产品代理销售在线工具.mp4"
+          url:"http://www.filefactory.com/file/2jtc8fcdwuhr/%u4EA7%u54C1%u4EE3%u7406%u9500%u552E%u5728%u7EBF%u5DE5%u5177.mp4"
         }             
     ];
     
