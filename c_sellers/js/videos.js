4d8fcfe4ffd8ddb244e3e5367684e392a6d31ce8
iansun <lauer3912@gmail.com>
2016-05-16 19:54:33 -0700

    更新

--- c_sellers/js/videos.js
+++ c_sellers/js/videos.js
@@ -1,36 +1,35 @@
 (function($) {
     
-    var _baseUrl = "/videos/sellers/"
     $.RTY_videos = [
         {
           category:"FaceBook", 
           name:"FaceBook上在小组内卖东西.mp4", 
           description:"在FaceBook上推广销售帖子及卖东西是一项有效的推广方式", 
-          url: _baseUrl + "/facebook/FaceBook上在小组内卖东西.mp4"
+          url: "https://www.youtube.com/embed/GOjl57b2v-Y"
         },
         {
           category:"FaceBook", 
           name:"FaceBook小组如何找到卖东西选项.mp4", 
           description:"有的时候，加入小组内卖东西选项找不到，可以这样试试", 
-          url:_baseUrl + "/facebook/FaceBook小组如何找到卖东西选项.mp4"
+          url: "https://www.youtube.com/embed/4VuzqAZ8nvk"
         },  
         {
           category:"FaceBook", 
           name:"FaceBook加入更多小组.mp4", 
           description:"好产品要推广，加入更多小组，以货品出售小组查找为例。", 
-          url:_baseUrl + "/facebook/FaceBook加入更多小组.mp4"
+          url:"https://www.youtube.com/embed/2d3d-UazS1k"
         },
         {
           category:"FaceBook", 
           name:"FaceBook如何添加更多小组.mp4", 
           description:"针对没有货品出售小组的同仁，可以这样添加更多小组", 
-          url:_baseUrl + "/facebook/FaceBook如何添加更多小组.mp4"
+          url:"https://www.youtube.com/embed/8sdQqZrDetQ"
         },      
         {
           category:"FaceBook", 
           name:"FaceBook小组WindowsMac如何将你的好友添加进来.mp4", 
           description:"我们自己的小组，希望将你的好友都申请加入进来，方便推广", 
-          url:_baseUrl + "/facebook/FaceBook小组WindowsMac如何将你的好友添加进来.mp4"
+          url:"https://www.youtube.com/embed/0Bgbd6YIWmo"
         },
             
             
@@ -39,7 +38,7 @@
           category:"Office", 
           name:"产品代理销售在线工具.mp4", 
           description:"如何使用产品在线销售工具?", 
-          url:_baseUrl + "/office/产品代理销售在线工具.mp4"
+          url:"https://www.youtube.com/embed/bRFmOhnmgFk"
         }             
     ];
     
