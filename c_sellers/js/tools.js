07532bfb934fd178fbd6342bf0ad543c22f3eef5
iansun <lauer3912@gmail.com>
2016-05-24 21:40:57 +0800

    更新

--- c_sellers/js/tools.js
+++ c_sellers/js/tools.js
@@ -4,7 +4,7 @@
         {
           category:"VPN", 
           name:"lantern 蓝灯VPN工具", 
-          description:"根据视频下载文件来安装蓝灯及使用蓝灯。可以解决访问FaceBook、YouTube、Google等网站", 
+          description:"根据视频下载文件来安装蓝灯及使用蓝灯。可以解决访问FaceBook、YouTube、Google等网站 http://www.filefactory.com/file/3io3j5uhx8wj/lantern-2.2.1.exe", 
           url: "http://www.filefactory.com/file/46dzpcup3aqr/VPN_lantern_setup.zip"
         }          
     ];
