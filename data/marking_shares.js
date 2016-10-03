39e59481a48412acaf351c7951137baeb1b4ddbb
iansun <lauer3912@gmail.com>
2016-10-03 08:47:30 +0800

    update

--- data/marking_shares.js
+++ data/marking_shares.js
@@ -17,25 +17,7 @@
         return $.extend(o, data);
     };
 
-    /// 软件
-    _U.data.push(_U.tp({
-        name: "蓝灯VPN Windows版本",
-        category: _U.category[0],
-        url:"https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta.exe",
-        comment:"微软操作系统Windows 版本(要求XP SP3以上) 电脑上使用,每月800M免费流量"
-    }));  
-    _U.data.push(_U.tp({
-        name: "蓝灯VPN macOS版本",
-        category: _U.category[0],
-        url:"https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta.exe",
-        comment:"苹果操作系统Windows 版本(要求XP SP3以上) 电脑上使用,每月800M免费流量"
-    }));    
-    _U.data.push(_U.tp({
-        name: "蓝灯VPN 安卓版本",
-        category: _U.category[1],
-        url:"https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta.apk",
-        comment:"安卓版(要求4.1以上) 手机上使用,每月800M免费流量"
-    }));  
+    /// 软件  
     _U.data.push(_U.tp({
         name: "XX-Net 桌面版本",
         category: _U.category[0],
