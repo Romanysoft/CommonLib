b99a7a019ae2b8ebaa945bbe96e89b48a4de46d0
iansun <lauer3912@gmail.com>
2016-10-23 15:50:10 +0800

    update

--- devhelper/zh-CN/js/app.toolbar.js
+++ devhelper/zh-CN/js/app.toolbar.js
@@ -20,7 +20,11 @@
 
         $('#toolbar').kendoToolBar({
             items:[
-                { type: "button", id:'toolBtn-apply', spriteCssClass: "k-tool-icon k-font-icon k-i-plus", text: "申请加入", enable: true, click: function(){_MC.send('app.showApplyWindow');}},
+                { type: "button", id:'toolBtn-apply', spriteCssClass: "k-tool-icon k-font-icon k-i-plus", text: "申请加入", enable: true, click: function(){
+                   //_MC.send('app.showApplyWindow');
+                   //<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1420836008&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2::53" alt="欢迎您咨询或申请加入我们！" title="欢迎您咨询或申请加入我们！"/></a>
+                   b$.App.open("http://wpa.qq.com/msgrd?v=3&uin=1420836008&site=qq&menu=yes")
+                }},
                 { type: "button", id:'toolBtn-pays', spriteCssClass: "k-tool-icon k-font-icon  k-i-dollar", text: "支付记录", enable: true, click: function(){_MC.send('app.showPayHistoryWindow');}},
                 { type: "button", id:'toolBtn-partners', spriteCssClass: "k-tool-icon k-font-icon k-i-format-number", text: "合作者名单", enable: true, click: function(){_MC.send('app.showPartnersWindow');}},
                 { type: "button", id:'toolBtn-resources', spriteCssClass: "k-tool-icon k-font-icon  k-i-sort-asc", text: "收益计算器", enable: true, click: function(){
@@ -30,6 +34,9 @@
                     b$.App.open('https://github.com/Romanysoft/working/wiki/Business-Cooperation');
                 }},                
                 { type: "button", id:'toolBtn-shares', spriteCssClass: "k-tool-icon k-font-icon   k-i-insert-file", text: "共享文件", enable: true, click: function(){_MC.send('app.showSharesWindow');}},
+                { type: "button", id:'toolBtn-feedback',  text: "有疑问？", enable: true, click: function(){
+                    b$.App.open("http://wpa.qq.com/msgrd?v=3&uin=1420836008&site=qq&menu=yes")
+                }},
                 { type: "separator", spriteCssClass: "flex-width" },
                 {template: tplStr }
             ]
--- markethelper/zh-CN/js/app.toolbar.js
+++ markethelper/zh-CN/js/app.toolbar.js
@@ -20,12 +20,18 @@
 
         $('#toolbar').kendoToolBar({
             items:[
-                { type: "button", id:'toolBtn-apply', spriteCssClass: "k-tool-icon k-font-icon k-i-plus", text: "申请加入", enable: true, click: function(){_MC.send('app.showApplyWindow');}},
+                { type: "button", id:'toolBtn-apply', spriteCssClass: "k-tool-icon k-font-icon k-i-plus", text: "申请加入", enable: true, click: function(){
+                    //_MC.send('app.showApplyWindow');
+                    b$.App.open("http://wpa.qq.com/msgrd?v=3&uin=1420836008&site=qq&menu=yes");
+                }},
                 { type: "button", id:'toolBtn-pays', spriteCssClass: "k-tool-icon k-font-icon  k-i-dollar", text: "支付记录", enable: true, click: function(){_MC.send('app.showPayHistoryWindow');}},
                 { type: "button", id:'toolBtn-partners', spriteCssClass: "k-tool-icon k-font-icon k-i-format-number", text: "合作者名单", enable: true, click: function(){_MC.send('app.showPartnersWindow');}},
                 { type: "button", id:'toolBtn-resources', spriteCssClass: "k-tool-icon k-font-icon  k-i-sort-asc", text: "资源分配记录", enable: true, click: function(){_MC.send('app.showResourcesWindow');}},
                 { type: "button", id:'toolBtn-shares', spriteCssClass: "k-tool-icon k-font-icon   k-i-insert-file", text: "共享文件", enable: true, click: function(){_MC.send('app.showSharesWindow');}},
-                { type: "button", id:'toolBtn-feedback',  text: "有疑问？", enable: true, click: function(){_MC.send('app.showFeedbackWindow');}},
+                { type: "button", id:'toolBtn-feedback',  text: "有疑问？", enable: true, click: function(){
+                    b$.App.open("http://wpa.qq.com/msgrd?v=3&uin=1420836008&site=qq&menu=yes")
+                    //_MC.send('app.showFeedbackWindow');
+                }},
                 { type: "separator", spriteCssClass: "flex-width" },
                 {template: tplStr }
             ]
