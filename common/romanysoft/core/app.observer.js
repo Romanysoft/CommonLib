b914f11fd4b9f8ce10a7f4f1f561d12274206e25
iansun <lauer3912@gmail.com>
2016-10-23 17:22:21 +0800

    升级 core

--- common/romanysoft/core/app.observer.js
+++ common/romanysoft/core/app.observer.js
@@ -1,5 +1,6 @@
 /**
  * Created by Ian on 2015/8/18.
+ *
  */
 
 (function () {
@@ -12,14 +13,81 @@
     c$ = $.extend(window.UI.c$, {});
     var b$ = window.BS.b$;
 
+    var useKendo = false;
+
     // 定义总线消息观察者
     c$.MessageCenter = {
-        obs:(new kendo.Observable()),
-        register:function(messageCode, fn, isOne){
-            !isOne ? this.obs.bind(messageCode, fn) : this.obs.one(messageCode, fn);
+
+        ///////////////////////////////////////////////////////
+        //For Jquery 特殊处理
+        obs_map_jquery:{},
+        fnc_once_map_jquery:{},
+        delegate: function (func, obj) {
+            var context = obj || null;
+            return function () {
+                func.apply(context, arguments);
+            }
         },
+
+        ////////////////////////////////////////////////////////
+        obs:(function(){
+            if(typeof kendo !== "undefined"){
+                if (typeof kendo.Observable !== "undefined"){
+                    useKendo = true;
+                }
+            }
+
+            return useKendo ? (new kendo.Observable()): {};
+        })(),
+
+        ////////////////////////////////////////////////////////
+        register:function(messageCode, fn, isOne){
+            var t$ = this;
+            if (useKendo){
+                !isOne ? t$.obs.bind(messageCode, fn) : t$.obs.one(messageCode, fn);
+            }else{
+
+                var obs = t$.obs_map_jquery[messageCode];
+                if(obs == undefined){
+                    obs = t$.obs_map_jquery[messageCode] = $.Callbacks();
+                }
+
+                var onceFncList = t$.fnc_once_map_jquery[messageCode];
+                if(onceFncList == undefined){
+                    onceFncList = t$.fnc_once_map_jquery[messageCode] = [];
+                }
+
+                var _fnc = t$.delegate(fn, null);
+                obs.add(_fnc);
+
+                if(isOne){
+                    onceFncList.push(_fnc);
+                }
+            }
+
+        },
+
+        ////////////////////////////////////////////////////////
         send:function(messageCode, data){
-            this.obs.trigger(messageCode, {"data":data ||{}});
+            var t$ = this;
+            if (useKendo){
+                t$.obs.trigger(messageCode, {"data":data ||{}});
+            }else{
+                var obs = t$.obs_map_jquery[messageCode];
+                if(obs){
+                    obs.fire(messageCode, {"data":data ||{}});
+
+                    var onceFncList = t$.fnc_once_map_jquery[messageCode];
+                    $.each(onceFncList, function(_, _fnc){
+                        if(obs.has(_fnc)){
+                            obs.remove(_fnc);
+                        }
+
+                        onceFncList.splice(_,1);
+                    })
+                }
+            }
+
         }
     };
 
--- common/romanysoft/core/app.util.js
+++ common/romanysoft/core/app.util.js
@@ -3,89 +3,95 @@
  */
 
 (function () {
-  window['UI'] = window['UI'] || {};
-  window.UI.c$ = window.UI.c$ || {};
+    window['UI'] = window['UI'] || {};
+    window.UI.c$ = window.UI.c$ || {};
 })();
 
 (function () {
-  var c$ = {};
-  c$ = $.extend(window.UI.c$, {});
-  var b$ = window.BS.b$;
-  var _MC = c$.MessageCenter; //
+    var c$ = {};
+    c$ = $.extend(window.UI.c$, {});
+    var b$ = window.BS.b$;
+    var _MC = c$.MessageCenter; //
 
-  /**
-   * 本地设置存储器
-   * @type {{obs: *, bind: c$.localSettingsWrap.bind, unbind: c$.localSettingsWrap.unbind, bindOnChange: c$.localSettingsWrap.bindOnChange, setItem: c$.localSettingsWrap.setItem, getItem: c$.localSettingsWrap.getItem}}
-   */
-  c$.localSettingsWrap = {
-    obs: (new kendo.Observable()),
-    bind: function (event, cb) {
-      this.obs.bind(event, cb || function (e) {
-          });
-    },
-    unbind: function (event, cb) {
-      this.obs.unbind(event, cb);
-    },
-    bindOnChange: function (cb) {
-      this.bind("onChange", cb || function (e) {
-          });
-    },
-    setItem: function (key, value) {
-      if ($.trim(key) === "") return;
+    /**
+     * 本地设置存储器
+     * @type {{obs: *, bind: c$.localSettingsWrap.bind, unbind: c$.localSettingsWrap.unbind, bindOnChange: c$.localSettingsWrap.bindOnChange, setItem: c$.localSettingsWrap.setItem, getItem: c$.localSettingsWrap.getItem}}
+     */
 
-      var jsonStrValue = JSON.stringify(value);
-      var oldValue = localStorage.getItem(key);
-      if (oldValue !== jsonStrValue) {
-        localStorage.setItem(key, jsonStrValue);
-        this.obs.trigger("onChange", {key: key, curValue: value, oldValue:oldValue});
-      }
-    },
+    /// 检测是否支持kendo
+    if(typeof kendo !== "undefined"){
+        c$.localSettingsWrap = {
+            obs: (new kendo.Observable()),
+            bind: function (event, cb) {
+                this.obs.bind(event, cb || function (e) {
+                    });
+            },
+            unbind: function (event, cb) {
+                this.obs.unbind(event, cb);
+            },
+            bindOnChange: function (cb) {
+                this.bind("onChange", cb || function (e) {
+                    });
+            },
+            setItem: function (key, value) {
+                if ($.trim(key) === "") return;
 
-    getItem: function (key) {
-      var value;
-      value = (value = localStorage.getItem(key)) && JSON.parse(value);
-      return value;
+                var jsonStrValue = JSON.stringify(value);
+                var oldValue = localStorage.getItem(key);
+                if (oldValue !== jsonStrValue) {
+                    localStorage.setItem(key, jsonStrValue);
+                    this.obs.trigger("onChange", {key: key, curValue: value, oldValue: oldValue});
+                }
+            },
+
+            getItem: function (key) {
+                var value;
+                value = (value = localStorage.getItem(key)) && JSON.parse(value);
+                return value;
+            }
+        };
     }
-  };
 
-  var f$ = {};
-  // 发现出错，弹出警告
-  f$.show_Dlg = function (info) {
-    var message = {
-      title: "Test Error",
-      message: info,
-      buttons: ["OK"],
-      alertType: "Information"
+
+
+    var f$ = {};
+    // 发现出错，弹出警告
+    f$.show_Dlg = function (info) {
+        var message = {
+            title: "Test Error",
+            message: info,
+            buttons: ["OK"],
+            alertType: "Information"
+        };
+        b$.Notice.alert(message);
     };
-    b$.Notice.alert(message);
-  };
 
-  // report Error
-  f$.reportError = function (error) {
-    var appName = BS.b$.App.getAppName();
-    var appVersion = BS.b$.App.getAppVersion();
-    var sn = BS.b$.App.getSerialNumber();
-    var info = BS.b$.App.getRegInfoJSONString();
-    var sandbox = BS.b$.App.getSandboxEnable();
-    $.reportInfo({
-      appName: appName,
-      version: appVersion,
-      sn: sn,
-      info: info,
-      sandbox: sandbox,
-      error: error
+    // report Error
+    f$.reportError = function (error) {
+        var appName = BS.b$.App.getAppName();
+        var appVersion = BS.b$.App.getAppVersion();
+        var sn = BS.b$.App.getSerialNumber();
+        var info = BS.b$.App.getRegInfoJSONString();
+        var sandbox = BS.b$.App.getSandboxEnable();
+        $.reportInfo({
+            appName: appName,
+            version: appVersion,
+            sn: sn,
+            info: info,
+            sandbox: sandbox,
+            error: error
+        });
+    };
+
+    //绑定可识别的消息
+    _MC.register("show_Dlg", function (e) {
+        f$.show_Dlg(e.data)
     });
-  };
-
-  //绑定可识别的消息
-  _MC.register("show_Dlg", function (e) {
-    f$.show_Dlg(e.data)
-  });
-  _MC.register("reportError", function (e) {
-    f$.reportError(e.data)
-  });
+    _MC.register("reportError", function (e) {
+        f$.reportError(e.data)
+    });
 
 
-  window.UI.c$ = $.extend(window.UI.c$, c$);
+    window.UI.c$ = $.extend(window.UI.c$, c$);
 })();
 
