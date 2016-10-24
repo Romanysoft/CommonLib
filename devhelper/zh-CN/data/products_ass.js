f25bd0ede250dce2390e8c0ac8c8ef8d754a1edb
iansun <lauer3912@gmail.com>
2016-10-24 17:04:50 +0800

    添加Demo

--- devhelper/zh-CN/data/products_ass.js
+++ devhelper/zh-CN/data/products_ass.js
@@ -7,6 +7,29 @@
     /// 本单元的处理
     var _U = {};
 
+    _U.types = ["上线", "Demo"]
+
+    ////练习
+    if(0){
+        _U.data.push({
+            productName:"",
+            srcdir:"",
+            appstoreurl:"",
+            demoName:"迟滞比较器计算",
+            demoUrl:"http://www.99cankao.com/electronic/chizhi.php",
+            partnerID:"D10161024"
+        });
+
+        _U.data.push({
+            productName:"",
+            srcdir:"",
+            appstoreurl:"",
+            demoName:"孔尺寸在线计算器",
+            demoUrl:"http://www.yunsuan.org/app/4197",
+            partnerID:"D9161024"
+        });        
+    }
+
 
     _U.data.push({
         productName:"",
@@ -14,6 +37,8 @@
         appstoreurl:"",
         partnerID:"D1160908"
     });
+
+
 
 
     return _U;
