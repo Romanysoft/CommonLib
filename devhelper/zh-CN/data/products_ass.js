f9597f3bdee29ac7fce72f91bf36f257c3be527e
iansun <lauer3912@gmail.com>
2016-10-24 19:31:42 +0800

    update

--- devhelper/zh-CN/data/products_ass.js
+++ devhelper/zh-CN/data/products_ass.js
@@ -6,30 +6,38 @@
 
     /// 本单元的处理
     var _U = {};
-
+    _U.data = [];
     _U.types = ["上线", "Demo"]
 
-    ////练习
-    if(1){
-        _U.data.push({
-            productName:"",
-            srcdir:"",
-            appstoreurl:"",
-            demoName:"迟滞比较器计算",
-            demoUrl:"http://www.99cankao.com/electronic/chizhi.php",
-            partnerID:"D10161024"
-        });
+    _U.data.push({
+        types:_U.types[0],
+        productName:"WheastoneBridgeCalculator",
+        srcdir:"testprj\1git_html\WheastoneBridgeCalculator",
+        appstoreurl:"https://itunes.apple.com/us/app/wheastonebridgecalculator/id1152151873?l=zh&ls=1&mt=12",
+        demoName:"WheastoneBridgeCalculator",
+        demoUrl:"http://www.99cankao.com/electronic/huisideng.php",
+        partnerID:"D8160908"
+    });
+  
+    _U.data.push({
+        types:_U.types[1],
+        productName:"",
+        srcdir:"",
+        appstoreurl:"",
+        demoName:"迟滞比较器计算",
+        demoUrl:"http://www.99cankao.com/electronic/chizhi.php",
+        partnerID:"D10161024"
+    });
 
-        _U.data.push({
-            productName:"",
-            srcdir:"",
-            appstoreurl:"",
-            demoName:"孔尺寸在线计算器",
-            demoUrl:"http://www.yunsuan.org/app/4197",
-            partnerID:"D9161024"
-        });        
-    }
-
+    _U.data.push({
+        types:_U.types[1],
+        productName:"",
+        srcdir:"",
+        appstoreurl:"",
+        demoName:"孔尺寸在线计算器",
+        demoUrl:"http://www.yunsuan.org/app/4197",
+        partnerID:"D9161024"
+    });
 
     return _U;
 
