7a70a105ffa61e66c63a75ee52805697249bc61b
iansun <lauer3912@gmail.com>
2016-10-24 21:00:14 +0800

    update

--- data/zh-CN/pays/E+C_A+B+C/base_201610.js
+++ data/zh-CN/pays/E+C_A+B+C/base_201610.js
@@ -19,25 +19,25 @@
     };
 
 
-    _U.data.push(_U.tp({id: "A019", visits: 200+16867, start:"2016/10/01", end:"2016/10/21"}));  
-    _U.data.push(_U.tp({id: "A020", visits: 200+68872, start:"2016/10/01", end:"2016/10/21"}));  
-    _U.data.push(_U.tp({id: "A021", visits: 200+140395, start:"2016/10/01", end:"2016/10/21"}));  
-    _U.data.push(_U.tp({id: "A022", visits: 200+31847, start:"2016/10/01", end:"2016/10/21"}));  
-    _U.data.push(_U.tp({id: "A023", visits: 200+53, start:"2016/10/09", end:"2016/10/21"}));
+    _U.data.push(_U.tp({id: "A019", visits: 200+16867, start:20161001, end:20161021}));  
+    _U.data.push(_U.tp({id: "A020", visits: 200+68872, start:20161001, end:20161021}));  
+    _U.data.push(_U.tp({id: "A021", visits: 200+140395, start:20161001, end:20161021}));  
+    _U.data.push(_U.tp({id: "A022", visits: 200+31847, start:20161001, end:20161021}));  
+    _U.data.push(_U.tp({id: "A023", visits: 200+53, start:20161009, end:20161021}));
 
     //2016.10.22
-    _U.data.push(_U.tp({id: "A019", visits: 2575, start:"2016/10/22", end:"2016/10/22"}));  
-    _U.data.push(_U.tp({id: "A020", visits: 3892, start:"2016/10/22", end:"2016/10/22"}));  
-    _U.data.push(_U.tp({id: "A021", visits: 3106, start:"2016/10/22", end:"2016/10/22"}));  
-    _U.data.push(_U.tp({id: "A022", visits: 2841, start:"2016/10/22", end:"2016/10/22"}));  
-    _U.data.push(_U.tp({id: "A023", visits: 5, start:"2016/10/22", end:"2016/10/22"}));    
+    _U.data.push(_U.tp({id: "A019", visits: 2575, start:20161022, end:20161022}));  
+    _U.data.push(_U.tp({id: "A020", visits: 3892, start:20161022, end:20161022}));  
+    _U.data.push(_U.tp({id: "A021", visits: 3106, start:20161022, end:20161022}));  
+    _U.data.push(_U.tp({id: "A022", visits: 2841, start:20161022, end:20161022}));  
+    _U.data.push(_U.tp({id: "A023", visits: 5, start:20161022, end:20161022}));    
 
     //2016.10.23
-    _U.data.push(_U.tp({id: "A019", visits: 1356, start:"2016/10/22", end:"2016/10/23"}));  
-    _U.data.push(_U.tp({id: "A020", visits: 1205, start:"2016/10/22", end:"2016/10/23"}));  
-    _U.data.push(_U.tp({id: "A021", visits: 1156, start:"2016/10/22", end:"2016/10/23"}));  
-    _U.data.push(_U.tp({id: "A022", visits: 96, start:"2016/10/22", end:"2016/10/23"}));  
-    _U.data.push(_U.tp({id: "A023", visits: 3, start:"2016/10/22", end:"2016/10/23"}));  
+    _U.data.push(_U.tp({id: "A019", visits: 1356, start:20161023, end:20161023}));  
+    _U.data.push(_U.tp({id: "A020", visits: 1205, start:20161023, end:20161023}));  
+    _U.data.push(_U.tp({id: "A021", visits: 1156, start:20161023, end:20161023}));  
+    _U.data.push(_U.tp({id: "A022", visits: 96, start:20161023, end:20161023}));  
+    _U.data.push(_U.tp({id: "A023", visits: 3, start:20161023, end:20161023}));  
 
     //2016.10.24
     
