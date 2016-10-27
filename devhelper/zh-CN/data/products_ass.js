381ec387e67449aecf81a1ec98bfd2217def495e
iansun <lauer3912@gmail.com>
2016-10-27 10:08:05 +0800

    update

--- devhelper/zh-CN/data/products_ass.js
+++ devhelper/zh-CN/data/products_ass.js
@@ -9,17 +9,18 @@
     _U.data = [];
     _U.types = ["上线", "Demo"]
 
+
     _U.data.push({
-        types:_U.types[0],
-        productName:"WheastoneBridgeCalculator",
-        srcdir:"testprj\1git_html\WheastoneBridgeCalculator",
-        devdir:"/Volumes/DataShareHFS/2013projects/Bus/p_WheastoneBridgeCalculator",
-        appstoreurl:"https://itunes.apple.com/us/app/wheastonebridgecalculator/id1152151873?l=zh&ls=1&mt=12",
-        demoName:"WheastoneBridgeCalculator",
-        demoUrl:"http://www.99cankao.com/electronic/huisideng.php",
-        partnerID:"D8160908"
+        types:_U.types[1],
+        productName:"",
+        srcdir:"",
+        appstoreurl:"",
+        demoName:"两点间距离图像计算器",
+        demoUrl:"http://www.99cankao.com/algebragraph/interactive-graph-distance.php",
+        partnerID:"D12161027"
     });
-  
+
+
     _U.data.push({
         types:_U.types[1],
         productName:"",
@@ -39,6 +40,17 @@
         demoUrl:"http://www.99cankao.com/colorconverter/color_close.php",
         partnerID:"D9161024"
     });
+
+    _U.data.push({
+        types:_U.types[0],
+        productName:"WheastoneBridgeCalculator",
+        srcdir:"testprj\1git_html\WheastoneBridgeCalculator",
+        devdir:"/Volumes/DataShareHFS/2013projects/Bus/p_WheastoneBridgeCalculator",
+        appstoreurl:"https://itunes.apple.com/us/app/wheastonebridgecalculator/id1152151873?l=zh&ls=1&mt=12",
+        demoName:"WheastoneBridgeCalculator",
+        demoUrl:"http://www.99cankao.com/electronic/huisideng.php",
+        partnerID:"D8160908"
+    });    
 
     return _U;
 
