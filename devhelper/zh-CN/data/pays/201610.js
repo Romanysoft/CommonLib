025880bb752bc39d27f3f8abd69aaff348b6d3c0
iansun <lauer3912@gmail.com>
2016-10-31 09:19:50 +0800

    合作开发工资20161029

--- devhelper/zh-CN/data/pays/201610.js
+++ devhelper/zh-CN/data/pays/201610.js
@@ -122,6 +122,26 @@
         _U.data.push(_U.tp({id: "D13161027", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));  
         _U.data.push(_U.tp({id: "D14161027", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));   
     }
+    
+    
+    if(1){
+        nowDate = 20161029;
+        curE = 6.77727;
+        _U.data.push(_U.tp({id: "D1150418", cost: 9.1, rate: curE, start:nowDate, end:nowDate}));  
+        _U.data.push(_U.tp({id: "D2150425", cost: 28.6, rate: curE, start:nowDate, end:nowDate}));  
+        _U.data.push(_U.tp({id: "D3150522", cost: 17.5, rate: curE, start:nowDate, end:nowDate}));  
+        _U.data.push(_U.tp({id: "D4150728", cost: 19.8, rate: curE, start:nowDate, end:nowDate}));  
+        _U.data.push(_U.tp({id: "D5150916", cost: 18.3, rate: curE, start:nowDate, end:nowDate}));  
+        _U.data.push(_U.tp({id: "D6151112", cost: 8.7, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D7151202", cost: 20.5, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D8160908", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));  
+        _U.data.push(_U.tp({id: "D9161024", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));  
+        _U.data.push(_U.tp({id: "D10161024", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));  
+        _U.data.push(_U.tp({id: "D11161024", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]})); 
+        _U.data.push(_U.tp({id: "D12161027", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));  
+        _U.data.push(_U.tp({id: "D13161027", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));  
+        _U.data.push(_U.tp({id: "D14161027", cost: 0, rate: curE, start:nowDate, end:nowDate, payedState:_U.payedState[3]}));   
+    }
 
 	window["rty_pays_dataobj"] = _U;
 	
