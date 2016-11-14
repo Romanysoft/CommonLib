acc7d1682002cf8e4edcd15b073394b9ed80e409
iansun <lauer3912@gmail.com>
2016-11-14 08:38:23 +0800

    20161112报酬

--- data/zh-CN/pays/E+C_A+B+C/base_201611.js
+++ data/zh-CN/pays/E+C_A+B+C/base_201611.js
@@ -113,6 +113,13 @@
     _U.data.push(_U.tp({id: "A021", visits: 1439, start:nowDate, end:nowDate}));
     _U.data.push(_U.tp({id: "A022", visits: 1753, start:nowDate, end:nowDate}));
 
+    //2016.11.13
+    nowDate = 20161113;
+    _U.data.push(_U.tp({id: "A019", visits: 3058, start:nowDate, end:nowDate}));
+    _U.data.push(_U.tp({id: "A020", visits: 2003, start:nowDate, end:nowDate}));
+    _U.data.push(_U.tp({id: "A021", visits: 1117, start:nowDate, end:nowDate}));
+    _U.data.push(_U.tp({id: "A022", visits: 1370, start:nowDate, end:nowDate}));
+
     window["rty_pays_dataobj"] = _U;
 
     return _U;
--- devhelper/zh-CN/data/pays/201611.js
+++ devhelper/zh-CN/data/pays/201611.js
@@ -247,6 +247,28 @@
         _U.data.push(_U.tp({id: "D16161109", cost: 0, rate: curE, start:nowDate, end:nowDate}));
     }
 
+    if(1){
+        nowDate = 20161112;
+        curE = 6.80911;
+        _U.data.push(_U.tp({id: "D1150418", cost: 16.7, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D2150425", cost: 9.8, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D3150522", cost: 30.8, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D4150728", cost: 20.3, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D5150916", cost: 22.8, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D6151112", cost: 44.5, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D7151202", cost: 19.4, rate: curE, start:nowDate, end:nowDate}));
+
+        _U.data.push(_U.tp({id: "D8160908", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D9161024", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D10161024", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D11161024", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D12161027", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D13161027", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D14161027", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D15161031", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+        _U.data.push(_U.tp({id: "D16161109", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+    }
+
 	window["rty_pays_dataobj"] = _U;
 
     return _U;
