de1c095254e96d790d965c8e26bebf5f740f948c
iansun <lauer3912@gmail.com>
2016-12-01 10:34:31 +0800

    20161130 update

--- data/zh-CN/pays/E+C_A+B+C/base_201611.js
+++ data/zh-CN/pays/E+C_A+B+C/base_201611.js
@@ -233,6 +233,13 @@
     _U.data.push(_U.tp({id: "A021", visits: 2318, start:nowDate, end:nowDate}));
     _U.data.push(_U.tp({id: "A022", visits: 1407, start:nowDate, end:nowDate}));
 
+    //2016.11.30
+    nowDate = 20161130;
+    _U.data.push(_U.tp({id: "A019", visits: 2567, start:nowDate, end:nowDate}));
+    _U.data.push(_U.tp({id: "A020", visits: 1501, start:nowDate, end:nowDate}));
+    _U.data.push(_U.tp({id: "A021", visits: 1394, start:nowDate, end:nowDate}));
+    _U.data.push(_U.tp({id: "A022", visits: 1023, start:nowDate, end:nowDate}));
+
     window["rty_pays_dataobj"] = _U;
 
     return _U;
