bda8f7ee2635ca612911985c9c1719193e7ee051
iansun <lauer3912@gmail.com>
2016-12-13 16:31:35 +0800

    update

--- devhelper/zh-CN/data/pays/201612.js
+++ devhelper/zh-CN/data/pays/201612.js
@@ -253,6 +253,29 @@
          _U.data.push(_U.tp({id: "D17161202", cost: 0, rate: curE, start:nowDate, end:nowDate}));
       }
 
+      if(1){
+         nowDate = 20161211;
+         curE = 6.90362;
+         _U.data.push(_U.tp({id: "D1150418", cost: 17.51, rate: curE, start:nowDate,  end:nowDate}));
+         _U.data.push(_U.tp({id: "D2150425", cost: 7.13, rate: curE, start:nowDate,  end:nowDate}));
+         _U.data.push(_U.tp({id: "D3150522", cost: 19.67, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D4150728", cost: 11.23, rate: curE, start:nowDate,  end:nowDate}));
+         _U.data.push(_U.tp({id: "D5150916", cost: 0, rate: curE, start:nowDate,  end:nowDate}));
+         _U.data.push(_U.tp({id: "D6151112", cost: 7.02, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D7151202", cost: 3.16, rate: curE, start:nowDate, end:nowDate}));
+
+         _U.data.push(_U.tp({id: "D8160908", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D9161024", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D10161024", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D11161024", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D12161027", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D13161027", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D14161027", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D15161031", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D16161109", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+         _U.data.push(_U.tp({id: "D17161202", cost: 0, rate: curE, start:nowDate, end:nowDate}));
+      }
+
       window["rty_pays_dataobj"] = _U;
 
       return _U;
